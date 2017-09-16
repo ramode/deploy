3da248be7bf4b80d9e67a1ffece53186a58f8d4b
eri <alex-eri@ya.ru>
2017-09-16 14:28:47 +0300

    pg

--- postgres.mk
+++ postgres.mk
@@ -1,8 +1,9 @@
 include ../run-billing-deploy/Makefile.vars
 
-${ROOT_DIR}/var/db: ${ROOT_DIR}/var
+${ROOT_DIR}/var/db/base: ${ROOT_DIR}/var
 	install -m 700 -d ${ROOT_DIR}/var/db/
 	[ -e  ${ROOT_DIR}/var/db/base ] || initdb ${ROOT_DIR}/var/db/
+	touch ${ROOT_DIR}/var/db/base
 
-runpg: ${ROOT_DIR}/var/db ${ROOT_DIR}/run
+runpg: ${POSTGRES} ${ROOT_DIR}/var/db/base ${ROOT_DIR}/run
 	${POSTGRES} -D ${ROOT_DIR}/var/db/ -c log_destination=stderr -c unix_socket_directories=${ROOT_DIR}/run/ &
